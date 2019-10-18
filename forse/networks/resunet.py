2dfa51e916d8b517c96465e7deef3bf37d7879ba
Nicoletta Krachmalnicoff <nicoletta.krachmalnicoff@sissa.it>
2019-10-18 06:46:25 -0700

    minor stylish in resunet

--- forse/networks/resunet.py
+++ forse/networks/resunet.py
@@ -1,7 +1,4 @@
-from forse.mmmtools import(
-                MinMaxRescale
-
-                )
+from forse.mmmtools import MinMaxRescale
 import numpy as np
 import tensorflow as tf
 from keras import layers
@@ -28,33 +25,28 @@ def conv_layer_block(X, f, filters, stage, block, s = 2):
     Returns:
     X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
     """
-    conv_id =   block + str(stage)
-    X= Dropout(rate=0.3, seed=stage, name='dropout_'+conv_id ) (X)
+    conv_id = block + str(stage)
+    X = Dropout(rate=0.3, seed=stage, name='dropout_'+conv_id ) (X)
     if s==1:
-        X = Conv2D( filters  ,  (f,f)  , padding='same',
-                   kernel_initializer = glorot_uniform(seed=0), name='conv_'+conv_id)(X)
+        X = Conv2D(filters, (f,f), padding='same', kernel_initializer=glorot_uniform(seed=0), 
+                name='conv_'+conv_id)(X)
     elif s==2:
-            X = Conv2D( filters  ,  (f,f) , strides=(s,s) , padding='same', name='conv_stride_'+conv_id)(X)
-
-    X=Activation('selu' ,  name='seLU_'+conv_id)(X)
-
+            X = Conv2D(filters, (f,f), strides=(s,s), padding='same', name='conv_stride_'+conv_id)(X)
+    X=Activation('selu' , name='seLU_'+conv_id)(X)
     X = BatchNormalization(axis=3, name='BN_'+conv_id) (X)
-
     return X
 
 def split_trainvaltest_sets(xraw):
-   nstamps=xraw.shape[-1]
-   npix =xraw.shape[0]
-   nchans=1
-   ntrains= int (nstamps *  4./5.)
-   nvals =   int (nstamps * 1./10.)
-   ntests =    int (nstamps * 1./10.)
-
-
-   return (
-   xraw[:,:,:ntrains].T.reshape(ntrains,npix,npix,  1)  ,
-   xraw[:,:, ntrains:ntrains + nvals].T.reshape(nvals,npix,npix,  1),
-  xraw[:,:,-ntests:].T.reshape(ntests,npix,npix,  1))
+    nstamps=xraw.shape[-1]
+    npix =xraw.shape[0]
+    nchans=1
+    ntrains= int (nstamps *  4./5.)
+    nvals =   int (nstamps * 1./10.)
+    ntests =    int (nstamps * 1./10.)
+    return (
+        xraw[:,:,:ntrains].T.reshape(ntrains,npix,npix,  1),
+        xraw[:,:, ntrains:ntrains + nvals].T.reshape(nvals,npix,npix,  1), 
+        xraw[:,:,-ntests:].T.reshape(ntests,npix,npix,  1))
 
 class ResUNet:
     def __init__(self, output_directory='./', img_size=64,
@@ -69,17 +61,16 @@ class ResUNet:
         self.batch_size=batch_size
         self.pretrained= pretrained
 
-    def load_model (self):
+    def load_model(self):
          json_file = open(self.model_directory+'/models/resunet_model.json', 'r')
          loaded_model_json = json_file.read()
          json_file.close()
          self.model = model_from_json(loaded_model_json)
-         self.model.compile(loss="mean_squared_error" ,
-                        optimizer = "Adam", metrics=['accuracy'])
+         self.model.compile(loss="mean_squared_error", optimizer = "Adam", metrics=['accuracy'])
          # load weights into new model
          self.model.load_weights(self.model_directory+"/models/resunet_model.h5")
-         if self.verbose: print("Loaded model from disk")
-
+         if self.verbose: 
+             print("Loaded model from disk")
 
     def load_training_set(self, patches_file):
         Y,X = np.load(patches_file)
@@ -91,97 +82,75 @@ class ResUNet:
             X[:,:,i] = MinMaxRescale(X[:,:,i], a=-1, b=1 )
         x_train,x_val, x_test = split_trainvaltest_sets(X)
         y_train,y_val, y_test = split_trainvaltest_sets(Y)
-        return x_train,x_val, x_test, y_train,y_val, y_test
+        return x_train, x_val, x_test, y_train,y_val, y_test
 
     def build_resunet(self):
         f=5
-        X_input =  Input(  (self.img_size,self.img_size, self.nchannels ) )
+        X_input = Input((self.img_size,self.img_size, self.nchannels ))
         #encoder
-        X= conv_layer_block(X_input,f, filters=64, stage=1, block='enc_', s=1) #64 x128
-        Xin=X
-
-        X= conv_layer_block(X ,f, filters=64, stage=2, block='enc_', s=1)
-        X= conv_layer_block(X ,f, filters=64, stage=3, block='enc_', s=1) #skip-conn 1
-
-        Xout1=X #64x128 add w/ input of 11-12
+        X = conv_layer_block(X_input,f, filters=64, stage=1, block='enc_', s=1) #64 x128
+        Xin = X
+        X = conv_layer_block(X ,f, filters=64, stage=2, block='enc_', s=1)
+        X = conv_layer_block(X ,f, filters=64, stage=3, block='enc_', s=1) #skip-conn 1
+        Xout1 = X #64x128 add w/ input of 11-12
         X = Add( )([Xin, X ]) #input layer 2  + input layer 4
-
-        Xin=X #64x128
-
-        X= conv_layer_block(X ,f, filters=128, stage=4, block='enc_', s=2) #128x64
-        X= conv_layer_block(X ,f, filters=128, stage=5, block='enc_', s=1)
-
-        Xin=Conv2D(128  ,  (f,f) , strides=(2,2) , padding='same', name='conv_input_1') (Xin) #128x 64
+        Xin = X #64x128
+        X = conv_layer_block(X ,f, filters=128, stage=4, block='enc_', s=2) #128x64
+        X = conv_layer_block(X ,f, filters=128, stage=5, block='enc_', s=1)
+        Xin = Conv2D(128, (f,f), strides=(2,2) , padding='same', name='conv_input_1')(Xin) #128x 64
         X = Add( )([Xin, X ]) #input layer 4  + input layer 6
-        Xin=X
-
-        X= conv_layer_block(X ,f, filters=128, stage=6, block='enc_', s=1) #skip-conn 2
-        Xout2=X  #combine w/ input 8-9
-
-        X= conv_layer_block(X ,f, filters=256, stage=7, block='enc_', s=2) #256 x 32
-        Xin=Conv2D(256  ,  (f,f) , strides=(2,2) , padding='same', name='conv_input_2')(Xin) #256x 32
-
-        X=  conv_layer_block(X ,f, filters=128, stage=8, block='dec_', s=1) #128 x32
-        X = UpSampling2D((2,2),interpolation='nearest', name='upsample_1')(X) #128x 64
+        Xin = X
+        X = conv_layer_block(X, f, filters=128, stage=6, block='enc_', s=1) #skip-conn 2
+        Xout2 = X  #combine w/ input 8-9
+        X = conv_layer_block(X ,f, filters=256, stage=7, block='enc_', s=2) #256 x 32
+        Xin = Conv2D(256, (f,f), strides=(2,2), padding='same', name='conv_input_2')(Xin) #256x 32
+        X =  conv_layer_block(X, f, filters=128, stage=8, block='dec_', s=1) #128 x32
+        X = UpSampling2D((2,2), interpolation='nearest', name='upsample_1')(X) #128x 64
         #skip-conn2
         X = Add( )([Xout2, X ])
-
-        Xin=X
-
-        X= conv_layer_block(X ,f, filters=128, stage=9, block='dec_', s=1)
-        X= conv_layer_block(X ,f, filters=128, stage=10, block='dec_', s=1)
+        Xin = X
+        X = conv_layer_block(X, f, filters=128, stage=9, block='dec_', s=1)
+        X = conv_layer_block(X, f, filters=128, stage=10, block='dec_', s=1)
         X = Add( )([Xin, X ]) #input layer 9  + input layer 11
-        Xin=X
-
-        X= conv_layer_block(X ,f, filters=64, stage=11, block='dec_', s=1) #64x64
-        X = UpSampling2D((2,2),interpolation='nearest', name='upsample_2' )(X) # 64 x 128
-
+        Xin = X
+        X = conv_layer_block(X, f, filters=64, stage=11, block='dec_', s=1) #64x64
+        X = UpSampling2D((2,2), interpolation='nearest', name='upsample_2')(X) # 64 x 128
         #skip-conn 1
         X = Add( )([Xout1, X ])
-
-        X= conv_layer_block(X ,f, filters=64, stage=12, block='dec_', s=1)
-
-        Xin= Conv2D(64  ,  (f,f) ,   padding='same', name='conv_input_3') (Xin)
-        Xin = UpSampling2D((2,2),interpolation='nearest', name='upsample_3' )(Xin) # 64 x 128
-
+        X = conv_layer_block(X, f, filters=64, stage=12, block='dec_', s=1)
+        Xin = Conv2D(64, (f,f), padding='same', name='conv_input_3')(Xin)
+        Xin = UpSampling2D((2,2), interpolation='nearest', name='upsample_3' )(Xin) # 64 x 128
         X = Add( )([Xin, X ]) #input layer 11  + input layer 13
-        Xin=X
+        Xin = X
         #skip-conn 1
-        X= conv_layer_block(X ,f, filters=64, stage=13, block='dec_', s=1)
-        X= conv_layer_block(X ,f, filters=64, stage=14, block='dec_', s=1)
+        X = conv_layer_block(X, f, filters=64, stage=13, block='dec_', s=1)
+        X = conv_layer_block(X, f, filters=64, stage=14, block='dec_', s=1)
         X = Add( )([Xin, X ]) #input layer 13 + input layer 15
-
-        Xin= Conv2D(1  ,  (f,f) ,  padding='same', name='conv_input_4') (X )
-
-
-        X= conv_layer_block(X ,f, filters=1, stage=15, block='dec_', s=1) #1x128
-        X= conv_layer_block(X ,f, filters=1, stage=16, block='dec_', s=1) #1x128
+        Xin = Conv2D(1, (f,f), padding='same', name='conv_input_4')(X)
+        X = conv_layer_block(X, f, filters=1, stage=15, block='dec_', s=1) #1x128
+        X = conv_layer_block(X, f, filters=1, stage=16, block='dec_', s=1) #1x128
         X = Add( )([Xin, X ]) #input layer 15  + last layer
-
-        self.model = Model(inputs = X_input, outputs = X, name='ResUNet')
+        self.model = Model(inputs=X_input, outputs=X, name='ResUNet')
         self.model.compile(loss='mean_squared_error',
-                        optimizer = "Adam",
+                        optimizer='Adam',
                         metrics=['accuracy'])
 
-
     def train(self, patches_file):
-
         if self.pretrained:
             self.load_model()
         else:
-            self.build_resunet( )
-        x_train, x_val,x_test, y_train,y_val, y_test = self.load_training_set(patches_file)
-
-        training= self.model.fit(x_train , y_train , epochs=self.epochs ,
-                                        batch_size=self.batch_size ,
-                                     shuffle=True, verbose=self.verbose ,
-                                     validation_data=(x_val ,y_val  ))
+            self.build_resunet()
+        x_train, x_val, x_test, y_train, y_val, y_test = self.load_training_set(patches_file)
+        training = self.model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size,
+                                     shuffle=True, verbose=self.verbose,
+                                     validation_data=(x_val, y_val))
         scores = self.model.evaluate(x_train, y_train, verbose=self.verbose)
-        if self.verbose : print( f"{self.model.metrics_names[1]} :  {scores[1]*100}" )
+        if self.verbose: 
+            print(f"{self.model.metrics_names[1]} :  {scores[1]*100}")
         save_path = self.model_directory + "/models"
         if not os.path.exists(save_path):
             os.makedirs(save_path)
-        for key in training.history.keys() :
+        for key in training.history.keys():
             np.save(save_path + f'/{key}_resunet{self.epochs}.npy', np.array (training.history[key]))
         model_json=  self.model.to_json()
         with open(save_path+'resunet_model.json', "w")  as json_file:
