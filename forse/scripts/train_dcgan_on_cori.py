abc180b9de7db0cb0023e866216100f8f9b134a4
Nicoletta Krachmalnicoff <nicoletta.krachmalnicoff@sissa.it>
2019-10-21 07:18:18 -0700

    import spectra estimator in this branch

---
+++ forse/scripts/train_dcgan_on_cori.py
@@ -0,0 +1,11 @@
+from forse.networks.dcgan import *
+import matplotlib.pyplot as plt
+from forse.tools.nn_tools import *
+from forse.tools.img_tools import *
+from forse.tools.mix_tools import *
+
+dcgan = DCGAN(output_directory='/global/homes/k/krach/scratch/NNforFG/DCGAN/tests/diff', img_size=(64, 64))
+training_path = '/global/homes/k/krach/scratch/NNforFG/training_set/'
+training_file = 'training_set_1000patches_20x20deg_T_HR1deg_LR5deg_Npix64_set2.npy'
+patch_file = training_path+training_file
+dcgan.train(epochs=100000, patches_file=patch_file, batch_size=32, save_interval=5000, swap=10)
