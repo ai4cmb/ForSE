1aa8ee3dd2d0d052fb35070ef525f005e0c4771b
Nicoletta Krachmalnicoff <nicoletta.krachmalnicoff@sissa.it>
2021-01-22 07:12:21 -0800

    notebook T

--- forse/scripts/train_dcgan_T.py
+++ forse/scripts/train_dcgan_T.py
@@ -1,4 +1,4 @@
-from forse.networks.dcgan_rnd import *
+from forse.networks.dcgan import *
 import matplotlib.pyplot as plt
 from forse.tools.nn_tools import *
 from forse.tools.img_tools import *
@@ -6,7 +6,7 @@ from forse.tools.mix_tools import *
 import tensorflow as tf
 
 dcgan = DCGAN(output_directory='./', img_size=(320, 320))
-training_path = '../datautils/datasets/'
+training_path = '/global/cfs/cdirs/sobs/www/users/ForSE/NN_datautils/datasets/'
 training_file = 'GNILC_Thr12_Tlr80_350patches_20x20deg_Npix320_mask8.npy'
 patch_file = training_path+training_file
 dcgan.train(epochs=100000, patches_file=patch_file, batch_size=16, save_interval=1000)
